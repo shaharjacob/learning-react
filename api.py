c90428b95120bc8079f6084d47a75e6988ed5763
huji520 <51635936+huji520@users.noreply.github.com>
2021-06-27 10:55:21 +0300

    .

--- api.py
+++ api.py
@@ -6,90 +6,94 @@ from utils import get_maximum_weighted_match, get_nodes_for_app, get_edges_for_a
 app = Flask(__name__)
 
 
-# we can render dict, tuple, or string
-@app.route("/hello-world-string")
-def hello_world_string():
-    return "Hello World"
+# # we can render dict, tuple, or string
+# @app.route("/")
+# def hello_world_string():
+#     return "Hello World"
+
+# @app.route("/hello-world-string")
+# def hello_world_string():
+#     return "Hello World 2"
 
 
-@app.route("/hello-world-dict")
-def hello_world_dict():
-    return {"key": "value"}
+# @app.route("/hello-world-dict")
+# def hello_world_dict():
+#     return {"key": "value"}
 
 
-# we can render html syntax (tag language)
-@app.route("/html-syntax")
-def html_syntax():
-    return """
-    <table border="1">
-        <tr>
-            <td>000</td>
-            <td>111</td>
-        </tr>
-        <tr>
-            <td>222</td>
-            <td>333</td>
-        </tr>
-    </table>
-    """
+# # we can render html syntax (tag language)
+# @app.route("/html-syntax")
+# def html_syntax():
+#     return """
+#     <table border="1">
+#         <tr>
+#             <td>000</td>
+#             <td>111</td>
+#         </tr>
+#         <tr>
+#             <td>222</td>
+#             <td>333</td>
+#         </tr>
+#     </table>
+#     """
 
-# we can render html page
-@app.route("/html-page")
-def html_page():
-    return render_template("example.html")
-
-
-@app.route("/query", methods=["GET", 'POST'])
-def query():
-    param1 = request.args.get('param1')
-    param2 = request.args.get('param2')
-    return {
-        "param1": param1,
-        "param2": param2
-    }
+# # we can render html page
+# @app.route("/html-page")
+# def html_page():
+#     return render_template("example.html")
 
 
-@app.route("/api", methods=["GET", 'POST'])
-def api():
-    param1 = request.args.get('param1')
-    param2 = request.args.get('param2')
-    # print("I'm here!!!")
-    return jsonify({
-        "my_string": f"{param1} {param2}",
-    })
+# @app.route("/query", methods=["GET", 'POST'])
+# def query():
+#     param1 = request.args.get('param1')
+#     param2 = request.args.get('param2')
+#     return {
+#         "param1": param1,
+#         "param2": param2
+#     }
 
 
-@app.route("/final-example", methods=["GET", "POST"])
-def final_example():
-    # left_side = request.args.get('left').split(',')
-    # right_side = request.args.get('right').split(',')
+# @app.route("/api", methods=["GET", 'POST'])
+# def api():
+#     param1 = request.args.get('param1')
+#     param2 = request.args.get('param2')
+#     # print("I'm here!!!")
+#     return jsonify({
+#         "my_string": f"{param1} {param2}",
+#     })
 
-    left_side = [
-        "earth revolve around the sun",
-        "earth circle around the sun",
-        "earth is far away from the sun",
-        "earh is smaller then the sun",
-        "sun is red",
-        "the color of earth is blue",
-        "this is a workshop in react",
-    ]
 
-    right_side = [
-        "earth orbit the sun",
-        "earth turn around the sun",
-        "earth is blue",
-        "no good match"
-    ]
+# @app.route("/final-example", methods=["GET", "POST"])
+# def final_example():
+#     # left_side = request.args.get('left').split(',')
+#     # right_side = request.args.get('right').split(',')
 
-    left_side_for_app = get_nodes_for_app(props=left_side, start_idx=0, x=200, group=0)
-    right_side_for_app = get_nodes_for_app(props=right_side, start_idx=len(left_side_for_app), x=800, group=1)
+#     left_side = [
+#         "earth revolve around the sun",
+#         "earth circle around the sun",
+#         "earth is far away from the sun",
+#         "earh is smaller then the sun",
+#         "sun is red",
+#         "the color of earth is blue",
+#         "this is a workshop in react",
+#     ]
 
-    similatiry_edges = get_maximum_weighted_match(left_side, right_side)
+#     right_side = [
+#         "earth orbit the sun",
+#         "earth turn around the sun",
+#         "earth is blue",
+#         "no good match"
+#     ]
 
-    return jsonify({
-        "nodes": left_side_for_app + right_side_for_app,
-        "edges": get_edges_for_app(similatiry_edges),
-    })
+#     left_side_for_app = get_nodes_for_app(props=left_side, start_idx=0, x=200, group=0)
+#     right_side_for_app = get_nodes_for_app(props=right_side, start_idx=len(left_side_for_app), x=800, group=1)
+
+#     similatiry_edges = get_maximum_weighted_match(left_side, right_side)
+
+#     return jsonify({
+#         "nodes": left_side_for_app + right_side_for_app,
+#         "edges": get_edges_for_app(similatiry_edges),
+#     })
 
 
 
--- webapp/src/components/graph/FinalExample.js
+++ webapp/src/components/graph/FinalExample.js
@@ -8,49 +8,50 @@ import Slider from '@material-ui/core/Slider';
 import '../main/Main.css'
 import './Graph.css'
 
+// example 1
+const FinalExample = () => {
 
-// const FinalExample = () => {
+    const [graph, setGraph] = useState({})
 
-//     const [graph, setGraph] = useState({})
+    const options = {
+        physics: {
+          enabled: false,
+        },
+        height: "800px",
+        groups: {
+          0: {color:{background:'orange'}, borderWidth:1},
+          1: {color:{background:'cyan'}, borderWidth:1}
+        }
+      };
 
-//     const options = {
-//         physics: {
-//           enabled: false,
-//         },
-//         height: "800px",
-//         groups: {
-//           0: {color:{background:'orange'}, borderWidth:1},
-//           1: {color:{background:'cyan'}, borderWidth:1}
-//         }
-//       };
+    useEffect(() => {
+        fetch('/final-example').then(response => {
+          if(response.ok){
+            return response.json()
+          }
+        }).then(data => {
+            setGraph(data)
+        })
+      },[])
 
-//     useEffect(() => {
-//         fetch('/final-example').then(response => {
-//           if(response.ok){
-//             return response.json()
-//           }
-//         }).then(data => {
-//             setGraph(data)
-//         })
-//       },[])
+    return (
+        <div className="main-container">
+            {graph
+            ?
+                <Graph
+                    graph={graph}
+                    options={options}
+                />
+            :
+                <></>
+            }
+        </div>
+    );
+}
 
-//     return (
-//         <div className="main-container">
-//             {graph
-//             ?
-//                 <Graph
-//                     graph={graph}
-//                     options={options}
-//                 />
-//             :
-//                 <></>
-//             }
-//         </div>
-//     );
-// }
+export default FinalExample;
 
-// export default FinalExample;
-
+// // example 2: slider for edges threshold
 // const FinalExample = () => {
 
 //     const [graph, setGraph] = useState({})
@@ -123,76 +124,78 @@ import './Graph.css'
 
 // export default FinalExample;
 
-const FinalExample = () => {
 
-  let location = useLocation()
-  const [graph, setGraph] = useState({})
+// // example 3: full demo
+// const FinalExample = () => {
 
-  const options = {
-    physics: {
-      enabled: false,
-    },
-    height: "800px",
-    groups: {
-      0: {color:{background:'orange'}, borderWidth:1},
-      1: {color:{background:'cyan'}, borderWidth:1}
-    }
-  };
+//   let location = useLocation()
+//   const [graph, setGraph] = useState({})
 
-  useEffect(() => {
-    let params = new URLSearchParams(location.search)
-    fetch(`/final-example?left=${params.get('left')}&right=${params.get('right')}`).then(response => {
-      if(response.ok){
-        return response.json()
-      }
-    }).then(data => {
-        setGraph(data)
-      })
-    },[])
+//   const options = {
+//     physics: {
+//       enabled: false,
+//     },
+//     height: "800px",
+//     groups: {
+//       0: {color:{background:'orange'}, borderWidth:1},
+//       1: {color:{background:'cyan'}, borderWidth:1}
+//     }
+//   };
+
+//   useEffect(() => {
+//     let params = new URLSearchParams(location.search)
+//     fetch(`/final-example?left=${params.get('left')}&right=${params.get('right')}`).then(response => {
+//       if(response.ok){
+//         return response.json()
+//       }
+//     }).then(data => {
+//         setGraph(data)
+//       })
+//     },[])
   
-  function valuetext(value) {
-    return `${value}`;
-  }
+//   function valuetext(value) {
+//     return `${value}`;
+//   }
 
-  function onThresholdChanged(event, value) {
-    let data = cloneDeep(graph)
-    for (let i = 0; i < data["edges"].length; i++){
-      let shouldBeHide = data["edges"][i]["value"] < value
-      data["edges"][i]["hidden"] = shouldBeHide
-    }
-    setGraph(data)
-  }
+//   function onThresholdChanged(event, value) {
+//     let data = cloneDeep(graph)
+//     for (let i = 0; i < data["edges"].length; i++){
+//       let shouldBeHide = data["edges"][i]["value"] < value
+//       data["edges"][i]["hidden"] = shouldBeHide
+//     }
+//     setGraph(data)
+//   }
 
-  return (
-    <div className="main-container">
-      {graph
-      ?
-      <>
-        <div className="slider">
-          <Slider
-          defaultValue={0.00}
-          getAriaValueText={valuetext}
-          aria-labelledby="discrete-slider-small-steps"
-          step={0.01}
-          min={0.00}
-          max={1.00}
-          valueLabelDisplay="on"
-          onChange={onThresholdChanged}
-          />
-          <span className="slider-title">
-            Similarity Threshold
-          </span>
-        </div>
-        <Graph
-          graph={graph}
-          options={options}
-        />
-      </>
-      :
-        <></>
-      }
-    </div>
-  );
-}
+//   return (
+//     <div className="main-container">
+//       {graph
+//       ?
+//       <>
+//         <div className="slider">
+//           <Slider
+//           defaultValue={0.00}
+//           getAriaValueText={valuetext}
+//           aria-labelledby="discrete-slider-small-steps"
+//           step={0.01}
+//           min={0.00}
+//           max={1.00}
+//           valueLabelDisplay="on"
+//           onChange={onThresholdChanged}
+//           />
+//           <span className="slider-title">
+//             Similarity Threshold
+//           </span>
+//         </div>
+//         <Graph
+//           graph={graph}
+//           options={options}
+//         />
+//       </>
+//       :
+//         <></>
+//       }
+//     </div>
+//   );
+// }
 
-export default FinalExample;
+// export default FinalExample;
